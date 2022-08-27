d61737c9396f24c562c7910cb7c81583e95abe98
Silvio Brändle <silvio@silviobraendle.ch>
2022-08-27 18:02:11 +0200

    Prevent non-standard touch gestures for counter buttons (UX)

--- src/apps/Counter.tsx
+++ src/apps/Counter.tsx
@@ -1,4 +1,4 @@
-import { h } from 'preact'
+import type { h } from 'preact'
 import { useState } from 'preact/hooks'
 
 import Typography from 'preact-material-components/Typography'
@@ -6,6 +6,8 @@ import Button from 'preact-material-components/Button'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+
+  const buttonStyle: h.JSX.CSSProperties = { touchAction: 'manipulation' }
 
   return (
     <div
@@ -22,13 +24,19 @@ const Counter = () => {
           gap: '1rem',
         }}
       >
-        <Button ripple raised onClick={() => setCount((count) => count + 1)}>
+        <Button
+          ripple
+          raised
+          onClick={() => setCount((count) => count + 1)}
+          style={buttonStyle}
+        >
           Increase
         </Button>
         <Button
           ripple
           outlined
           onClick={() => setTimeout(() => setCount((count) => count + 1), 2000)}
+          style={buttonStyle}
         >
           Increase Async (2s)
         </Button>
@@ -39,13 +47,19 @@ const Counter = () => {
           gap: '1rem',
         }}
       >
-        <Button ripple raised onClick={() => setCount((count) => count - 1)}>
+        <Button
+          ripple
+          raised
+          onClick={() => setCount((count) => count - 1)}
+          style={buttonStyle}
+        >
           Decrease
         </Button>
         <Button
           ripple
           outlined
           onClick={() => setTimeout(() => setCount((count) => count - 1), 2000)}
+          style={buttonStyle}
         >
           Decrease Async (2s)
         </Button>
